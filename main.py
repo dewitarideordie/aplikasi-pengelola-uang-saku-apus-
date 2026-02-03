e7015b80c69cf2ce665135d61343d74dec76cb50
rideordie <diajengdewita@gmail.com>
2026-02-03 07:02:51 +0000

    apes

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+import json
+import os
+
 saldo = 0
+FILE_DATA = "data.json"
+
+def simpan_data():
+    global saldo
+    data = {"saldo": saldo}
+    with open(FILE_DATA, "w") as file:
+        json.dump(data, file)
+
+def muat_data():
+    global saldo
+    if os.path.exists(FILE_DATA):
+        with open(FILE_DATA, "r") as file:
+            data = json.load(file)
+            saldo = data.get("saldo", 0)
+        return saldo
+    return 0
 
 def tambah_pemasukan():
     global saldo
     jumlah = int(input("Masukkan jumlah pemasukan: "))
     saldo = saldo + jumlah
+    simpan_data()
     print(f"Pemasukan sebesar {jumlah} berhasil ditambahkan!")
 
 def tambah_pengeluaran():
-    pass
+    global saldo
+    jumlah = int(input("Masukkan jumlah pengeluaran: "))
+    if jumlah > saldo:
+        print("Saldo tidak cukup!")
+    else:
+        saldo = saldo - jumlah
+        simpan_data()
+        print(f"Pengeluaran sebesar {jumlah} berhasil dicatat!")
 
 def lihat_saldo():
-    pass
+    print("\n" + "="*40)
+    print("SALDO SAAT INI".center(40))
+    print("="*40)
+    print(f"{'Saldo':<20} | Rp. {saldo:>10,}".replace(",", "."))
+    print("="*40 + "\n")
 
 def menu():
     print("=== Aplikasi Pengelola Uang Saku ===")
@@ -18,6 +49,8 @@ def menu():
     print("2. Tambah pengeluaran")
     print("3. Lihat saldo")
     print("4. Keluar")
+
+saldo = muat_data()
 
 while True:
     menu()
