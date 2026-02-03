88a6ddfbc28d3fd2207105d062d6aefb998cbfdf
rideordie <diajengdewita@gmail.com>
2026-02-03 06:57:56 +0000

    apes

---
+++ main.py
@@ -0,0 +1,36 @@
+saldo = 0
+
+def tambah_pemasukan():
+    global saldo
+    jumlah = int(input("Masukkan jumlah pemasukan: "))
+    saldo = saldo + jumlah
+    print(f"Pemasukan sebesar {jumlah} berhasil ditambahkan!")
+
+def tambah_pengeluaran():
+    pass
+
+def lihat_saldo():
+    pass
+
+def menu():
+    print("=== Aplikasi Pengelola Uang Saku ===")
+    print("1. Tambah pemasukan")
+    print("2. Tambah pengeluaran")
+    print("3. Lihat saldo")
+    print("4. Keluar")
+
+while True:
+    menu()
+    pilihan = input("Pilih menu: ")
+
+    if pilihan == "1":
+        tambah_pemasukan()
+    elif pilihan == "2":
+        tambah_pengeluaran()
+    elif pilihan == "3":
+        lihat_saldo()
+    elif pilihan == "4":
+        print("Terima kasih!")
+        break
+    else:
+        print("Pilihan tidak valid")
